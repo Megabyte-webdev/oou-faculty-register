274131bbc0560c435f769acfd8323d60180c4343
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2025-02-16 02:12:29 +0100

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,9 +4,9 @@ import Computer from "../assets/pngs/computer.png";
 import Dialog from "../assets/pngs/dialog.png";
 //import { Helmet } from "react-helmet";
 import Calenderr from "../assets/pngs/calender.png";
-
+import {useNavigate} from 'react-router-dom'
 function Dashboard() {
- 
+ const navigate=useNavigate();
   return (
     <div className="flex flex-wrap gap-5 w-full min-h-full px-4 md:px-[2%] pt-[1%]">
   {/* <Helmet>
@@ -24,14 +24,14 @@ function Dashboard() {
         />
       </li>
 
-      <li className="h-60 w-full bg-white rounded-[10px] flex flex-col items-center justify-center overflow-hidden cursor-pointer group hover:scale-105 transition-transform duration-150">
+      <li onClick={()=>navigate("/dashboard/attendance")} className="h-60 w-full bg-white rounded-[10px] flex flex-col items-center justify-center overflow-hidden cursor-pointer group hover:scale-105 transition-transform duration-150">
         <img className="w-[80px] md:w-[100px]" src={Computer} alt="" />
         <span className="text-green text-sm">Enter Class Room</span>
       </li>
 
-      <li className="h-60 w-full bg-[brown] rounded-[10px] flex flex-col items-center justify-center overflow-hidden cursor-pointer hover:scale-105 transition-transform duration-150">
+      <li onClick={()=>navigate("/dashboard/courses")} className="h-60 w-full bg-[brown] rounded-[10px] flex flex-col items-center justify-center overflow-hidden cursor-pointer hover:scale-105 transition-transform duration-150">
         <img className="w-[80px] md:w-[100px]" src={Dialog} alt="" />
-        <span className="text-white text-sm">Forum</span>
+        <span className="text-white text-sm">Courses</span>
       </li>
 
       <li className="h-60 w-full bg-blue-500 rounded-[10px] flex flex-col items-center justify-center overflow-hidden cursor-pointer hover:scale-105 transition-transform duration-150">
